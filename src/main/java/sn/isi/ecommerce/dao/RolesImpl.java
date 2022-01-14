e48787d76729ccbd878237ff14773281e0dceafe
Exaucé <47887636+exauce21@users.noreply.github.com>
2022-01-14 18:35:47 +0000

    Test JUnit Role et Utilisateur

--- src/main/java/sn/isi/ecommerce/dao/RolesImpl.java
+++ src/main/java/sn/isi/ecommerce/dao/RolesImpl.java
@@ -1,2 +1,71 @@
-package sn.isi.ecommerce.dao;public class RolesImpl {
+package sn.isi.ecommerce.dao;
+
+import sn.isi.ecommerce.entities.Roles;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class RolesImpl implements IRoles{
+    private EntityManager em;
+    public RolesImpl() {
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("ecommercePU");
+        em = emf.createEntityManager();
+    }
+
+    @Override
+    public int add(Roles role) {
+        try {
+            em.getTransaction().begin();
+            em.persist(role);
+            em.getTransaction().commit();
+            return 1;
+        }catch(Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Override
+    public int update(Roles role) {
+        try {
+            em.getTransaction().begin();
+            em.merge(role);
+            em.getTransaction().commit();
+            return 1;
+        }catch(Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Override
+    public int delete(int id) {
+        try {
+            em.getTransaction().begin();
+            em.remove(em.find(Roles.class, id));
+            em.getTransaction().commit();
+            return 1;
+        }catch(Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Override
+    public Roles get(int id) {
+        return em.find(Roles.class, id);
+    }
+
+    @Override
+    public List<Roles> getAll() {
+        try {
+            return em.createNamedQuery("roles.all").getResultList();
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/main/java/sn/isi/ecommerce/dao/UsersImpl.java
+++ src/main/java/sn/isi/ecommerce/dao/UsersImpl.java
@@ -1,2 +1,86 @@
-package sn.isi.ecommerce.dao;public class UsersImpl {
+package sn.isi.ecommerce.dao;
+
+import sn.isi.ecommerce.entities.Users;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class UsersImpl implements IUsers{
+
+    private EntityManager em;
+
+    public UsersImpl() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ecommercePU");
+        em = emf.createEntityManager();
+    }
+
+    @Override
+    public int add(Users user) {
+        try {
+            em.getTransaction().begin();
+            em.persist(user);
+            em.getTransaction().commit();
+            return 1;
+        }catch(Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Override
+    public int update(Users user) {
+        try {
+            em.getTransaction().begin();
+            em.merge(user);
+            em.getTransaction().commit();
+            return 1;
+        }catch(Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Override
+    public int delete(int id) {
+        try {
+            em.getTransaction().begin();
+            em.remove(em.find(Users.class, id));
+            em.getTransaction().commit();
+            return 1;
+        }catch(Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Override
+    public Users get(int id) {
+        return em.find(Users.class, id);
+    }
+
+    @Override
+    public List<Users> getAll() {
+        try {
+            //return em.createQuery("SELECT u FROM User u").getResultList();
+            return em.createNamedQuery("user.all").getResultList();
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public Users login(String username, String password) {
+        try {
+            return (Users) em.createNamedQuery("user.logon")
+                    .setParameter("email", username)
+                    .setParameter("password", password)
+                    .getSingleResult();
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
