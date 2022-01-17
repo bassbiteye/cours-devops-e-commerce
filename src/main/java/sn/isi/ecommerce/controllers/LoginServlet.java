64014bb8580b3d5d2755effd2074a921a43ad8c0
thiernoKayfo98 <thierno@kayfo.games>
2022-01-17 00:24:39 +0000

    Intégration template admin

--- src/main/java/sn/isi/ecommerce/controllers/LoginServlet.java
+++ src/main/java/sn/isi/ecommerce/controllers/LoginServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet("Login")
+@WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private IUsers userdao;
