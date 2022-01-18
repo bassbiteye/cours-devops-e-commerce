c43248903cb8efbc49c93cde38c966469e206aa8
Exaucé <47887636+exauce21@users.noreply.github.com>
2022-01-18 21:57:08 +0000

    Gestion des sessions des utilisateurs

--- src/test/java/Tests/RolesImplTest.java
+++ src/test/java/Tests/RolesImplTest.java
@@ -22,7 +22,7 @@ class RolesImplTest {
         IRoles roledoa = new RolesImpl();
 
         Roles roles = new Roles();
-        roles.setNom("ROLE_SUPADMIN");
+        roles.setNom("ROLE_GESTIONNAIRE");
 
         int result = roledoa.add(roles);
         System.out.println(result);
