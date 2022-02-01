88a32b5066f2cd3f6dfd6acc7e2dcd8b3f93831f
Badoulahi8 <badoulahi8@gmail.com>
2022-02-01 18:05:17 +0000

    Fin test details commande

--- src/test/java/sn/isi/ecommerce/dao/DetailImplTest.java
+++ src/test/java/sn/isi/ecommerce/dao/DetailImplTest.java
@@ -1,5 +1,6 @@
 package sn.isi.ecommerce.dao;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import sn.isi.ecommerce.entities.Commande;
@@ -36,32 +37,35 @@ class DetailImplTest {
 
     @Test
     void deleteDetail() {
-        ok = detailDao.deleteDetail(1); ;
-        if(ok == 0)
-        {
-            System.out.println("Echec Suppression Detail commande");
-        }
-        else
-        {
-            System.out.println("Supression reussie");
-        }
+        Assertions.assertNotNull(detailDao.deleteDetail(1), "Echec Suppression d'un detail commande");
+
+//        ok = detailDao.deleteDetail(1); ;
+//        if(ok == 0)
+//        {
+//            System.out.println("Echec Suppression Detail commande");
+//        }
+//        else
+//        {
+//            System.out.println("Supression reussie");
+//        }
     }
 
     @Test
     void getDetail() {
-        detail = detailDao.getDetail(1) ;
-        if(detail == null)
-        {
-            System.out.println("Deatil inexistante");
-        }
-        else
-        {
-            System.out.println("Get detail reussie");
-        }
+//        detail = detailDao.getDetail(1) ;
+//        if(detail == null)
+//        {
+//            System.out.println("Deatil inexistante");
+//        }
+//        else
+//        {
+//            System.out.println("Get detail reussie");
+//        }
+        Assertions.assertNotNull(detailDao.getDetail(1), "Echec recuperation d'un detail commande");
     }
 
     @Test
     void getAllDetail() {
-
+        Assertions.assertNotNull( detailDao.getAllDetail(), "La methode getAllDetails a echoué") ;
     }
 }
