a7dd9b319081d1c29b53e034c80db673e4688bdd
Samband14 <samband456@gmail.com>
2022-02-14 15:31:06 +1200

    inteface-Facturation
    Facturation

---
+++ src/main/java/sn/isi/ecommerce/dao/IFacturation.java
@@ -0,0 +1,10 @@
+package sn.isi.ecommerce.dao;
+
+import sn.isi.ecommerce.entities.Client;
+import sn.isi.ecommerce.entities.Commande;
+
+import java.util.List;
+
+public interface IFacturation {
+
+}
