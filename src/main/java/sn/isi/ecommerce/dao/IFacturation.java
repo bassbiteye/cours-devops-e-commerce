73b99f9c9bc4a39c8018c4ed1fa99de9352cc1c4
moustapha-blsq <moustaphalkarim92@gmail.com>
2022-02-15 09:58:51 +0000

    Mis a jour IFacture et creation de la classe d'implementation FactureImple

--- src/main/java/sn/isi/ecommerce/dao/IFacturation.java
+++ src/main/java/sn/isi/ecommerce/dao/IFacturation.java
@@ -2,9 +2,13 @@ package sn.isi.ecommerce.dao;
 
 import sn.isi.ecommerce.entities.Client;
 import sn.isi.ecommerce.entities.Commande;
+import sn.isi.ecommerce.entities.Facturation;
 
 import java.util.List;
 
 public interface IFacturation {
+    List<Facturation> getAllFacture();
+    Facturation saveFacture(Client client, Commande commande);
+
 
 }
